2f527323dc53aa9bf7f2887ef545eb38fd6a5179
CppDevVS2026 <CppDevVS2026@outlook.com>
2025-12-12 09:42:03 +0200

    refactor: move `calculate_shift_force` to `calculators` module and integrate `gilrs` in `main.rs`

---
+++ src/calculators.rs
@@ -0,0 +1,7 @@
+/// Calculate the shift force
+pub fn calculate_shift_force(current_torque: u32, max_torque: u32) -> u32 {
+    let force = (current_torque as f32 / max_torque as f32) * 100f32;
+
+    // Clamp between 0 and 100
+    f32::floor(force.clamp(0f32, 100f32)) as u32
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-fn main() {
-    println!("Current Shift Force: {}", calculate_shift_force(500, 900))
-}
-/// Calculate the shift force
-fn calculate_shift_force(current_torque: u32, max_torque: u32) -> u32 {
-    let force = (current_torque as f32 / max_torque as f32) * 100f32;
+mod calculators;
 
-    // Clamp between 0 and 100
-    f32::floor(force.clamp(0f32, 100f32)) as u32
+use gilrs::{Button, Event, Gilrs};
+
+fn main() {
+    let mut gilrs = Gilrs::new().unwrap();
+
+    for (_id, gamepad) in gilrs.gamepads() {
+        println!("{:?} ")
+    }
 }
